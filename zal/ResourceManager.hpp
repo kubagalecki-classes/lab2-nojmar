9f766398335fcf53f8ee68c3dba38d7c348d7885
nojmar <73884817+nojmar@users.noreply.github.com>
2020-11-11 15:45:40 +0100

    Update ResourceManager.hpp

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,5 +4,17 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    private:
+    Resource* resource;
+    
+    public:
+    ResourceManager(){
+    this->resource=new Resource{};
+    }
+    ~ResourceManager(){
+    delete resource;
+    }
+    double get(){
+    return resource->get();
+    }
 };
